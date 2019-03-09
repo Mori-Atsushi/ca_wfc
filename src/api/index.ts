239ead984fbc174440dda944a2f27ee9754b8992
Mori Atsushi <mori@cyder.tech>
2019-03-10 01:41:03 +0900

    add result

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,6 @@ import { getRequest } from 'api/common';
 import { IImagesResponse, IImage } from 'api/response';
 
 export async function getList(): Promise<IImage[]> {
-  return getRequest<IImagesResponse>('/images')
+  return getRequest<IImagesResponse>('/images?limit=100')
     .then(result => result.data.images);
 }
